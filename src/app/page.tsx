af7604b77b65be413eec03f3c88c805b54d18776
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2023-04-16 19:04:21 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,103 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client';
+import React, { useState, useEffect } from 'react';
+import { Select, Button, List } from 'antd';
+import styles from './page.module.css';
+import { useDebounce } from 'use-debounce'; // Import useDebounce hook
 
-const inter = Inter({ subsets: ['latin'] })
+const { Option } = Select;
 
-export default function Home() {
+const IndexPage: React.FC = () => {
+  const [subreddits, setSubreddits] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [selectedSubs, setSelectedSubs] = useState<string[]>([]);
+  const [timeframe, setTimeframe] = useState<string>('1d');
+  const [posts, setPosts] = useState<string[]>([]);
+
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 1000); // Use useDebounce hook with handleSearch function
+  useEffect(
+    () => {
+      handleSearch(debouncedSearchTerm);
+    },
+    [debouncedSearchTerm] // Only call effect if debounced search term changes
+  );
+
+  async function handleSearch(value: string) {
+    try {
+      // Fetch subreddits from Reddit API based on search query
+      const response = await fetch(
+        `https://www.reddit.com/subreddits/search.json?q=${value}`
+      );
+      const data = await response.json();
+      // Extract subreddit names from API response and update subreddits state
+      const extractedSubreddits =
+        data?.data?.children?.map((child: any) => child?.data?.display_name) ||
+        [];
+      console.log(extractedSubreddits);
+      setSubreddits(extractedSubreddits);
+    } catch (error) {
+      console.error('Error fetching subreddits from Reddit API:', error);
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      // Fetch data from Reddit API based on selected subreddits and timeframe
+      const response = await fetch(
+        `https://www.reddit.com/r/${subreddits.join(
+          '+'
+        )}/top.json?t=${timeframe}`
+      );
+      const data = await response.json();
+      // Extract posts from API response and update posts state
+      const extractedPosts = data?.data?.children?.map(
+        (child: any) => child?.data?.title
+      );
+      setPosts(extractedPosts);
+    } catch (error) {
+      console.error('Error fetching data from Reddit API:', error);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.app}>
+      <h1 className={styles.title}>Reddit App</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor='subreddit' className={styles.label}>
+          Select Subreddit:
+        </label>
+        <Select
+          id='subreddit'
+          className={styles.select}
+          mode='multiple' // Enable multiple selections
+          value={selectedSubs}
+          onSearch={(value) => setSearchTerm(value)} // Fetch subreddits based on search query
+          onChange={(value) => setSelectedSubs(value)}
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {/* Render subreddit options from subreddits state */}
+          {subreddits.map((sub, index) => (
+            <Option key={index} value={sub}>
+              {sub}
+            </Option>
+          ))}
+        </Select>
+        {/* Render input fields for timeframe selection */}
+        {/* ... */}
+        <Button type='primary' htmlType='submit' className={styles.button}>
+          Fetch Data
+        </Button>
+      </form>
+      {/* Render the fetched posts */}
+      <List
+        dataSource={posts}
+        renderItem={(post, index) => (
+          <List.Item key={index} className={styles.post}>
+            {post}
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default IndexPage;
